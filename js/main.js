1482f755ce41d2e6f02ba24be9250caf9883def9
janeth-montero <janeth.montero08@gmail.com>
2023-08-23 12:35:21 -0700

    Clase 3: header, config de secciones y avance boton flotante

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+console.log('Hola mundo!');
+
+
+function mostrarMensajeInicial() {
+
+  if (confirm('Este sitio esta en desarrollo, disculpe las molestias')) {
+    alert('Gracias por visitarnos!');
+  } else {
+    alert('...');
+  }
+
+}
